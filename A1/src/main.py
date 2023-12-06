9a430760838593df8bb317c92c6c94a4a49b1b15
menglutao <azraeltml@gmail.com>
2023-12-07 01:40:51 +0200

    Add LSTM network structure

--- A1/src/main.py
+++ A1/src/main.py
@@ -7,6 +7,96 @@ from collections import Counter
 import tensorflow as tf
 from utils.activity_type import ActivityType
 import numpy as np
+tf.compat.v1.disable_eager_execution()
+
+
+# TODO: move class Config to a separate file
+class Config(object):
+    """
+    define a class to store parameters,
+    the input should be feature mat of training and testing
+
+    Note: it would be more interesting to use a HyperOpt search space:
+    https://github.com/hyperopt/hyperopt
+    """
+
+    def __init__(self, X_train, X_test):
+        # Input data
+        self.train_count = len(X_train)  # 99006 training series
+        self.test_data_count = len(X_test)  # 44461 testing series
+        self.n_steps = len(X_train[0])  # 128 time_steps per series
+
+        # Training
+        self.learning_rate = 0.0025
+        self.lambda_loss_amount = 0.0015
+        self.training_epochs = 300
+        self.batch_size = 1500
+
+        # LSTM structure
+        self.n_inputs = len(X_train[0][0])  # Features count is of 12 features over time
+        self.n_hidden = 32  # nb of neurons inside the neural network
+        self.n_classes = 6  # Final output classes
+        self.W = {
+            'hidden': tf.Variable(tf.random.normal([self.n_inputs, self.n_hidden])),
+            'output': tf.Variable(tf.random.normal([self.n_hidden, self.n_classes]))
+        }
+        self.biases = {
+            'hidden': tf.Variable(tf.random.normal([self.n_hidden], mean=1.0)),
+            'output': tf.Variable(tf.random.normal([self.n_classes]))
+        }
+
+
+def LSTM_Network(_X, config):
+    """Function returns a TensorFlow RNN with two stacked LSTM cells
+
+    Two LSTM cells are stacked which adds deepness to the neural network.
+    Note, some code of this notebook is inspired from an slightly different
+    RNN architecture used on another dataset, some of the credits goes to
+    "aymericdamien".
+
+    Args:
+        _X:     ndarray feature matrix, shape: [batch_size, time_steps, n_inputs]
+        config: Config for the neural network.
+
+    Returns:
+        This is a description of what is returned.
+
+    Raises:
+        KeyError: Raises an exception.
+
+      Args:
+        feature_mat: ndarray fature matrix, shape=[batch_size,time_steps,n_inputs]
+        config: class containing config of network
+      return:
+              : matrix  output shape [batch_size,n_classes]
+    """
+    # (NOTE: This step could be greatly optimised by shaping the dataset once
+    # input shape: (batch_size, n_steps, n_input)
+    _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
+    # Reshape to prepare input to hidden activation
+    _X = tf.reshape(_X, [-1, config.n_inputs])
+    # new shape: (n_steps*batch_size, n_input)
+
+    # Linear activation
+    _X = tf.nn.relu(tf.matmul(_X, config.W['hidden']) + config.biases['hidden'])
+    # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    _X = tf.split(_X, config.n_steps, 0)
+    # new shape: n_steps * (batch_size, n_hidden)
+
+    # Define two stacked LSTM cells (two recurrent layers deep) with tensorflow
+    lstm_cell_1 = tf.compat.v1.nn.rnn_cell.BasicLSTMCell(config.n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_2 = tf.compat.v1.nn.rnn_cell.BasicLSTMCell(config.n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cells = tf.compat.v1.nn.rnn_cell.MultiRNNCell([lstm_cell_1, lstm_cell_2], state_is_tuple=True)
+    # Get LSTM cell output
+    outputs, states = tf.compat.v1.nn.static_rnn(lstm_cells, _X, dtype=tf.float32)
+
+    # Get last time step's output feature for a "many to one" style classifier,
+    # as in the image describing RNNs at the top of this page
+    lstm_last_output = outputs[-1]
+
+    # Linear activation
+    return tf.matmul(lstm_last_output, config.W['output']) + config.biases['output']
+
 
 def main():
 
@@ -33,18 +123,71 @@ def main():
     one_hot_encoded_train_y = ActivityType.one_hot(train_data_y, label_mapping).reshape(-1,6)
     one_hot_encoded_test_y = ActivityType.one_hot(test_data_y, label_mapping)
     final_train_y = one_hot_encoded_train_y.reshape(one_hot_encoded_train_y.shape[0],-1)
+    final_test_y = one_hot_encoded_test_y.reshape(one_hot_encoded_test_y.shape[0],-1)
 
-    print("Train Data X:",train_data_X.shape)
-    print("Train Data y:",train_data_y.shape)
-    # print("Test Data X:",test_data_X.shape)
-    # print("Test Data y:",test_data_y.shape)
-    # see the distribution of labels
-    # print(Counter(one_hot_encoded_test_y.reshape(-1)))
-    # print(one_hot_encoded_test_y[1167:1190])
+    config = Config(train_data_X, test_data_X)
+    print("Some useful info to get an insight on dataset's shape and normalisation:")
+    print("features shape, labels shape, each features mean, each features standard deviation")
+    print(test_data_X.shape, one_hot_encoded_test_y.shape,
+          np.mean(test_data_X), np.std(test_data_X))
+    print("the dataset is therefore properly normalised, as expected.")
 
-    print(train_data_X.shape, final_train_y.shape,
-          np.mean(train_data_X), np.std(train_data_X))
-    
+
+    # ------------------------------------------------------
+    # Step 3: Let's get serious and build the neural network
+    # ------------------------------------------------------
+
+    X = tf.compat.v1.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
+    Y = tf.compat.v1.placeholder(tf.float32, [None, config.n_classes])
+
+    pred_Y = LSTM_Network(X, config)
+
+    # # Loss,optimizer,evaluation
+    l2 = config.lambda_loss_amount * \
+        sum(tf.nn.l2_loss(tf_var) for tf_var in tf.compat.v1.trainable_variables())
+    # Softmax loss and L2
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=tf.stop_gradient(Y), logits=pred_Y)) + l2
+    optimizer = tf.compat.v1.train.AdamOptimizer(
+        learning_rate=config.learning_rate).minimize(cost)
+
+    correct_pred = tf.equal(tf.argmax(pred_Y, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
+
+    # --------------------------------------------
+    # Step 4: Hooray, now train the neural network
+    # --------------------------------------------
+
+    # Note that log_device_placement can be turned ON but will cause console spam with RNNs.
+    sess = tf.compat.v1.InteractiveSession(config=tf.compat.v1.ConfigProto(log_device_placement=False))
+    init = tf.compat.v1.global_variables_initializer()
+    sess.run(init)
+
+    best_accuracy = 0.0
+    # Start training for each batch and loop epochs
+    for i in range(config.training_epochs):
+        for start, end in zip(range(0, config.train_count, config.batch_size),
+                              range(config.batch_size, config.train_count + 1, config.batch_size)):
+            sess.run(optimizer, feed_dict={X: train_data_X[start:end],
+                                           Y: final_train_y[start:end]})
+
+        # Test completely at every epoch: calculate accuracy
+        pred_out, accuracy_out, loss_out = sess.run(
+            [pred_Y, accuracy, cost],
+            feed_dict={
+                X: test_data_X,
+                Y: final_test_y
+            }
+        )
+        print("traing iter: {},".format(i) +
+              " test accuracy : {},".format(accuracy_out) +
+              " loss : {}".format(loss_out))
+        best_accuracy = max(best_accuracy, accuracy_out)
+
+    print("")
+    print("final test accuracy: {}".format(accuracy_out))
+    print("best epoch's test accuracy: {}".format(best_accuracy))
+    print("")
 
 
 
